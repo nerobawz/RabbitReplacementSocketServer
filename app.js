d8b547ea4e261ffbdf7bb4fb7ab6f1d4f020fd2f
Rene <r.vanvliet@student.fontys.nl>
2019-08-16 02:38:21 +0200

    cleanup: const instead of var and removed unused code

--- app.js
+++ app.js
@@ -10,67 +10,17 @@ if(!production){
     });
 }
 else{
-    var fs = require( 'fs' );
-    var app = require('express')();
-    var https        = require('https');
-    var server = https.createServer({
+    const fs = require('fs');
+    const app = require('express')();
+    const https = require('https');
+    const server = https.createServer({
         key: fs.readFileSync('/etc/letsencrypt/live/chocoloco.tk/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/chocoloco.tk/fullchain.pem')
-    },app);
+    }, app);
     server.listen(5000);
 
     io = require('socket.io').listen(server);
 }
-
-
-/*
-
-
-*/
-// io.sockets.on('connection',function (socket) {
-//   console.log("user connected");
-//
-//   // Log whenever a client disconnects from our websocket server
-//   socket.on("disconnect", function() {
-//     console.log("user disconnected");
-//   });
-//
-//   // When we receive a 'message' event from our client, print out
-//   // the contents of that message and then echo it back to our client
-//   // using `io.emit()`
-//   socket.on("SEND_CHAT_MESSAGE", message => {
-//     console.log("SEND_CHAT_MESSAGE Received: " + message);
-//     io.emit("message", { type: "SEND_CHAT_MESSAGE", text: message });
-//   });
-//
-//   socket.on("SEND_VIDEO_TIME", message => {
-//     console.log("SEND_VIDEO_TIME Received: " + message);
-//     io.emit("message", { type: "SEND_VIDEO_TIME", text: message });
-//   });
-//
-//   socket.on("PLAY_VIDEO", message => {
-//     console.log("PLAY_VIDEO Received: " + message);
-//     io.emit("message", { type: "PLAY_VIDEO", text: message });
-//   });
-//
-//   socket.on("PAUSE_VIDEO", message => {
-//     console.log("PAUSE_VIDEO Received: " + message);
-//     io.emit("message", { type: "PAUSE_VIDEO", text: message});
-//   });
-//
-//   socket.on("CURRENT_TIME", message => {
-//     // console.log("CURRENT_TIME Received: " + message);
-//     io.emit("message", { type: "CURRENT_TIME", text: message});
-//   });
-//
-//   socket.on("SET_VIDEO_URL", message => {
-//     console.log("SET_VIDEO_URL Received: " + message);
-//     io.emit("message", { type: "SET_VIDEO_URL", text: message});
-//   });
-// });
-
-
-
 
 let currentWatchingVideoUrl = 'https://chocoloco.tk/big_buck_bunny.mp4';
 
