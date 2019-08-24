be6fb35d0570d379364f83fdfe4c82041cb7ad4e
Rene <r.vanvliet@student.fontys.nl>
2019-08-24 15:24:29 +0200

    added chat history storage

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const production = process.env.NODE_ENV === 'Production';
 let io;
+var fs = require('fs');
+const chatlogFile = 'histoooory';
+var logStreamW = fs.createWriteStream(chatlogFile, {'flags': 'a'});
+var logStreamR = fs.createReadStream(chatlogFile);
+let chatMessages = [];
+readLines(logStreamR,func);
+
+
 if(!production){
     let app = require("express")();
     let http = require("http").Server(app);
@@ -29,7 +37,7 @@ io.on("connection", socket => {
     let id;
     // Log whenever a user connects
     console.log("user connected", socket.id);
-
+    io.to(socket.id).emit("message",{type:"SET_USER_ID",text:'{"type":11,"text":"'+socket.id+'"}'});
     // Send the new client the current url.
     io.to(socket.id).emit("message", { type: "SET_VIDEO_URL", text: '{"type":5,"text":"'+currentWatchingVideoUrl+'"}'});
 
@@ -37,14 +45,15 @@ io.on("connection", socket => {
         console.log("JOIN_ROOM Received: " + message);
         username = JSON.parse(message)['user'];
         id = JSON.parse(message)['id'];
-        io.emit("message", { type: "JOIN_ROOM", text: message });
+        io.to(socket.id).emit("message", { type: "GET_HISTORY", text: '{"type":12,"text":'+JSON.stringify(chatMessages)+'}'});
+        socket.broadcast.emit("message", { type: "JOIN_ROOM", text: message });
     });
 
 
     // Log whenever a client disconnects from our websocket server
     socket.on("disconnect", function(reason) {
         console.log("user disconnected " + username + " " + reason);
-        io.emit("message", { type: "LEAVE_ROOM", text: '{"type":7,"text":"Test message","id":"'+id+'","user":"'+username+'"}'});
+        socket.broadcast.emit("message", { type: "LEAVE_ROOM", text: '{"type":7,"text":"Test message","id":"'+socket.id+'","user":"'+username+'"}'});
     });
 
 
@@ -53,48 +62,86 @@ io.on("connection", socket => {
     // using `io.emit()`
     socket.on("SEND_CHAT_MESSAGE", message => {
         console.log("SEND_CHAT_MESSAGE Received: " + message);
-        io.emit("message", { type: "SEND_CHAT_MESSAGE", text: message });
+        chatMessages.push(message.toString());
+        logStreamW.write(message + '\n');
+        socket.broadcast.emit("message", { type: "SEND_CHAT_MESSAGE", text: message });
     });
 
     socket.on("SEND_VIDEO_TIME", message => {
         console.log("SEND_VIDEO_TIME Received: " + message);
-        io.emit("message", { type: "SEND_VIDEO_TIME", text: message });
+        socket.broadcast.emit("message", { type: "SEND_VIDEO_TIME", text: message });
+    });
+
+    socket.on("GET_HISTORY", message => {
+        console.log("GET_HISTORY Received: " + message);
+        io.to(socket.id).emit("message", { type: "GET_HISTORY", text: message });
     });
 
     socket.on("PLAY_VIDEO", message => {
         console.log("PLAY_VIDEO Received: " + message);
-        io.emit("message", { type: "PLAY_VIDEO", text: message });
+        socket.broadcast.emit("message", { type: "PLAY_VIDEO", text: message });
     });
 
     socket.on("PAUSE_VIDEO", message => {
         console.log("PAUSE_VIDEO Received: " + message);
-        io.emit("message", { type: "PAUSE_VIDEO", text: message});
+        socket.broadcast.emit("message", { type: "PAUSE_VIDEO", text: message});
     });
 
     socket.on("STARTED_TYPING", message => {
         console.log("STARTED_TYPING Received: " + message);
-        io.emit("message", { type: "STARTED_TYPING", text: message});
+        socket.broadcast.emit("message", { type: "STARTED_TYPING", text: message});
     });
 
     socket.on("STOPPED_TYPING", message => {
         console.log("STOPPED_TYPING Received: " + message);
-        io.emit("message", { type: "STOPPED_TYPING", text: message});
+        socket.broadcast.emit("message", { type: "STOPPED_TYPING", text: message});
     });
 
     socket.on("CHANGED_USERNAME", message => {
         console.log("CHANGED_USERNAME Received: " + message);
-        io.emit("message", { type: "CHANGED_USERNAME", text: message});
+        socket.broadcast.emit("message", { type: "CHANGED_USERNAME", text: message});
     });
 
     socket.on("CURRENT_TIME", message => {
         // console.log("CURRENT_TIME Received: " + message);
-        io.emit("message", { type: "CURRENT_TIME", text: message});
+        socket.broadcast.emit("message", { type: "CURRENT_TIME", text: message});
     });
 
     socket.on("SET_VIDEO_URL", message => {
         console.log("SET_VIDEO_URL Received: " + message);
         currentWatchingVideoUrl = JSON.parse(message)['text'];
-        io.emit("message", { type: "SET_VIDEO_URL", text: message});
+        socket.broadcast.emit("message", { type: "SET_VIDEO_URL", text: message});
     });
 
 });
+
+
+function readLines(input, func) {
+    console.log('readlines:');
+    var remaining = '';
+
+    input.on('data', function(data) {
+        remaining += data;
+        var index = remaining.indexOf('\n');
+        var last  = 0;
+        while (index > -1) {
+            var line = remaining.substring(last, index);
+            last = index + 1;
+            func(line);
+            index = remaining.indexOf('\n', last);
+        }
+
+        remaining = remaining.substring(last);
+    });
+
+    input.on('end', function() {
+        if (remaining.length > 0) {
+            func(remaining);
+        }
+    });
+}
+
+
+function func(data) {
+    chatMessages.push(data);
+}
